026b31c9256c85f7d246b5155662cfd139259a01
juwangju <>
2022-07-05 14:23:38 +0800

    项目转移github 初次提交

---
+++ example/android/app/src/main/java/com/baidu/flutter_bmflocation_example/MyApplication.java
@@ -0,0 +1,11 @@
+package com.example.flutter_bmflocation_example;
+
+import com.baidu.mapapi.base.BmfMapApplication;
+
+public class MyApplication extends BmfMapApplication{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
